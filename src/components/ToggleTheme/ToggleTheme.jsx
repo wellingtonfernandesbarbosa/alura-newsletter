ed2b6c80ba0c1c43852ed6de2b225792d3faaacc
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-10-17 21:48:20 -0300

    Implementação do Tema Escuro
    Neste commit, o tema escuro foi implementado no projeto, estendendo as cores do Tailwind CSS para incluir tons de escuro e personalizando a paleta de cores para se adequar ao tema escuro.

--- src/components/ToggleTheme/ToggleTheme.jsx
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -1,9 +1,17 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
 
 export default function ToggleTheme() {
+  const systemPreference = window.matchMedia('(prefers-color-scheme: dark').matches;
+  const pagesClasses = document.documentElement.classList;
+
+  useEffect(() => {
+    systemPreference && pagesClasses.add('dark');
+  })
   const toggle = () => {
-    document.documentElement.classList.toggle('dark')
+    pagesClasses.toggle('dark')
   }
+  
   return <div className="hidden sm:block">
     <MoonIcon className="h-8 text-gray-100 cursor-pointer block dark:hidden" onClick={toggle}/>
     <SunIcon className="h-8 text-gray-100 cursor-pointer hidden dark:block" onClick={toggle}/>
