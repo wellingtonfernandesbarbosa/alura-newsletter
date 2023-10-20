88a6aec775832146d0cd50a6a1a4190c225fc06c
Wellington Fernandes <wellingtonfernandes@protonmail.com>
2023-10-19 23:50:47 -0300

    Implementada a adição de imagem e texto alternativo nos posts quando disponível

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,27 +1,25 @@
-import PropTypes from 'prop-types';
-
-Article.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.arrayOf(PropTypes.string).isRequired,
-  tags: PropTypes.arrayOf(PropTypes.string),
-  image: PropTypes.string,
-  alt: PropTypes.string,
-};
-
+/* eslint-disable react/prop-types */
 const Article = ({ title, text, tags, image, alt }) => {
   return (
     <div className="p-5 bg-gray-300 flex flex-col gap-3 items-center dark:bg-dark-200 sm:rounded-lg sm:shadow-lg">
-      <h3 className="text-xl text-alura-200 font-bold text-center dark:text-gray-400">{title}</h3>
+      <h3 className="text-xl text-alura-200 font-bold text-center dark:text-gray-400">
+        {title}
+      </h3>
       <div className="grid gap-1">
         {text.map((content, index) => (
-          <p className="text-alura-200 dark:text-gray-400 text-justify" key={index}>{content}</p>
+          <p
+            className="text-alura-200 dark:text-gray-400 text-justify"
+            key={index}
+          >
+            {content}
+          </p>
         ))}
       </div>
+      {image && <img className="sm:p-4" src={image} />}
+      {image && alt && (
+        <span className="text-alura-200 dark:text-gray-400 sr-only">{alt}</span>
+      )}
     </div>
   );
 };
-
-
-
-
 export default Article;
