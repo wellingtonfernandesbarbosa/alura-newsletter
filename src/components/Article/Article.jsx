8ad27ab1438a807eff1e3eac2c291225c097b36b
Wellington Fernandes <wellingtonfernandes@protonmail.com>
2023-10-23 14:39:36 -0300

    Melhorias nos Estilos para Melhorar Usabilidade
    Neste commit, foram realizadas melhorias nos estilos dos componentes para aprimorar a usabilidade do projeto. Foram aplicadas alterações nos estilos de elementos como tags, cabeçalho e artigos para proporcionar uma experiência mais agradável aos usuários.

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
 const Article = ({ title, text, tags, image, alt }) => {
   return (
-    <div className="p-5 bg-gray-300 flex flex-col gap-3 items-center dark:bg-dark-200 sm:rounded-lg sm:shadow-lg">
+    <div className="p-5 bg-gray-300 flex flex-col gap-3 items-center dark:bg-dark-200 sm:rounded-lg sm:shadow-lg sm:hover:shadow-gray-500 sm:hover:dark:shadow-gray-800">
       <h3 className="text-xl text-alura-200 font-bold text-center dark:text-gray-400">
         {title}
       </h3>
       <div className="w-full justify-end gap-2 hidden sm:flex ">
         {tags.map((tag) => (
-          <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase">{tag}</span>
+          <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs hover:bg-alura-200 dark:hover:bg-alura-100 hover:scale-110 uppercase">{tag}</span>
         ))}
       </div>
-      <div className="grid gap-1">
+      <div className="grid gap-2">
         {text.map((content, index) => (
           <p
-            className="text-alura-200 dark:text-gray-400 text-justify"
+            className="text-alura-200 dark:text-gray-400"
             key={index}
           >
             {content}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ user }) {
   return (
     <div className="p-5 w-full sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
       <span className="text-gray-100">Olá, {user?.name || "Usuário"}</span>
-      <h1 className="text-gray-100 text-2xl sm:text-4xl hover:scale-110">
+      <h1 className="text-gray-100 text-xl sm:hover:text-2xl sm:hover:text-xs-2 sm:cursor-pointer">
         Alura Newsletter
       </h1>
       <ToggleTheme />
