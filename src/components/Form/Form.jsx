43decd4dda9ca9951c5066efae865549d517dc04
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-10-26 07:13:13 -0300

    Aprimoramentos nos Estilos
    Neste commit, foram feitos aprimoramentos nos estilos do projeto, incluindo a adição de gradiente nas tags, efeitos de hover suaves no botão de submissão do formulário e a aplicação do estilo 'caret' aos campos de input, melhorando a experiência visual e de interação.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,7 +14,7 @@ export default function Form({ onSubmit }) {
     <form onSubmit={safeSubmit} className="flex flex-col gap-10 justify-center items-center h-full -mt-20 mx-5">
       <Input type="text" placeholder="Insira seu nome..." required/>
       <Input type="email" placeholder="Insira seu email..." required/>
-      <button type="submit" className="w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 py-1 px-5 uppercase outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 dark:hover:shadow-black">Enviar</button>
+      <button type="submit" className="w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 py-1 px-5 uppercase outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 dark:hover:shadow-black hover:-translate-y-1 hover:delay-200">Enviar</button>
     </form>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 export default function Header({ user }) {
   return (
-    <div className="p-5 w-full sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
+    <div className="p-5 sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
       <span className="text-gray-100">Olá, {user?.name || "Usuário"}</span>
       <h1 className="text-gray-100 text-xl sm:hover:text-2xl sm:hover:text-xs-2 sm:cursor-pointer">
         Alura Newsletter
